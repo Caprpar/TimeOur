7ff39bde55b52658a29706f29f6eb842c624e6d5
caspar <cappetjatte@gmail.com>
2025-10-21 11:44:05 +0200

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 
 const app = express(),
   port = process.env.PORT || 3000;
 
 const frontend = path.join(path.resolve(), "dist");
+
+app.use(cors());
 
 app.get("/api", (_request, response) => {
   response.send({ hello: "world" });
